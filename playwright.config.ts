a4fad91df6d15ec909af5b0d3c9dbfff4c2d6d02
Ahmed Davila Guerrero <c-aguerrero@kahua.com>
2022-06-22 17:51:34 -0400

    Initial commit

---
+++ playwright.config.ts
@@ -0,0 +1,13 @@
+import { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  use: {
+    headless: false,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    video: 'on-first-retry',
+    browserName: 'chromium',
+  },
+};
+
+export default config;
